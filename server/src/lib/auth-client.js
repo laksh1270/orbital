42d3d6c2337f06005c2a4928cafa8a7c5082e926
laksh1270 <114750613+laksh1270@users.noreply.github.com>
2025-12-23 19:55:22 +0530

    Delete server/src/lib/auth-client.js

--- server/src/lib/auth-client.js
+++
@@ -1,7 +0,0 @@
-import { createAuthClient } from "better-auth/client";
-import { deviceAuthorizationClient } from "better-auth/client/plugins";
-
-export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_AUTH_URL!,
-  plugins: [deviceAuthorizationClient()],
-});
