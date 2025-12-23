c7e0a95547291bf22e6b158675b0e6b038c39947
laksh1270 <114750613+laksh1270@users.noreply.github.com>
2025-12-23 18:15:28 +0530

    Refactor authClient to use NEXT_PUBLIC_AUTH_URL
    Updated authClient to use environment variable for baseURL.

--- server/src/lib/auth-client.js
+++ server/src/lib/auth-client.js
@@ -1,9 +1,11 @@
-import { deviceAuthorizationClient } from "better-auth/client/plugins"
-import { createAuthClient } from "better-auth/client"
+import { createAuthClient } from "better-auth/client";
+import { deviceAuthorizationClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3005",
-      plugins: [ 
-    deviceAuthorizationClient(), 
-  ], 
-})
+  // ✅ Backend URL (Render)
+  baseURL: process.env.NEXT_PUBLIC_AUTH_URL,
+
+  plugins: [
+    deviceAuthorizationClient(),
+  ],
+});
